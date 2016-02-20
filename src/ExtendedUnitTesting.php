1b3020be307400ca04d6dae543cfd39b8b26e504
Quentin Renard <contact@asticode.com>
2016-02-20 19:23:05 +0100

    Updated call method for unit testing

--- src/ExtendedUnitTesting.php
+++ src/ExtendedUnitTesting.php
@@ -5,10 +5,10 @@ use ReflectionClass;
 
 class ExtendedUnitTesting
 {
-    public static function callMethod($oObject, $sMethodName, array $aArgs = []) {
-        $class = new ReflectionClass($oObject);
-        $method = $class->getMethod($sMethodName);
-        $method->setAccessible(true);
-        return $method->invokeArgs($oObject, $aArgs);
+    public static function callMethod($sClassName, $sMethodName, array $aArgs = [], $oObject = null) {
+        $oReflectionClass = new ReflectionClass($sClassName);
+        $oReflectionMethod = $oReflectionClass->getMethod($sMethodName);
+        $oReflectionMethod->setAccessible(true);
+        return $oReflectionMethod->invokeArgs($oObject, $aArgs);
     }
 }
